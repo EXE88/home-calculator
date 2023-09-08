e3ba901e23b372b4acf04881db7425d9b2e80f2d
danial <bobolk1388@gmail.com>
2023-09-09 00:19:20 +0430

    complete login serializer and adding meta class because serializer.is_valid need that , create func is completed too

--- users/serializers.py
+++ users/serializers.py
@@ -1,12 +1,29 @@
 from rest_framework import serializers
+from django.contrib.auth import authenticate,login
+from django.contrib.auth.models import User
 
 class UserLoginSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username','password']
+    
     username = serializers.CharField(max_length=50,min_length=5,required=True,allow_blank=False)
     password = serializers.CharField(max_length=50,min_length=8,required=True,allow_blank=False)
 
-class UserRegisterSerializers(serializers.ModelSerializer):
+    def create(self, validated_data):
+        username = validated_data.get("username")
+        password = validated_data.get("password")
+        
+        user = authenticate(username=username,password=password)
+        
+        if user is not None:
+            login(self.context['request'],user)
+            return user
+        else:
+            return serializers.ValidationError('invalid username or password')
+
+class UserRegisterSerializers(serializers.ModelSerializer):   
     username = serializers.CharField(max_length=50,min_length=5,required=True,allow_blank=False)
     email = serializers.EmailField(required=True,allow_blank=False)
     password = serializers.CharField(max_length=50,min_length=8,required=True,allow_blank=False)
     confirm_password = serializers.CharField(max_length=50,min_length=8,required=True,allow_blank=False)
-    
