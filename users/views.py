0f75bdeb6bd0c72334537cccc867e44dce19e728
danial <bobolk1388@gmail.com>
2023-09-09 00:21:54 +0430

    write view for user login

--- users/views.py
+++ users/views.py
@@ -6,7 +6,11 @@ from .serializers import UserLoginSerializers,UserRegisterSerializers
 
 class UserLoginView(APIView):
     def post(self,request):
-        pass
+        serializer = UserLoginSerializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response('user logedin successfully',status=status.HTTP_200_OK)
+        return Response('invalid data',status=status.HTTP_400_BAD_REQUEST)
 
 class UserRegisterView(APIView):
     def post(self,request):
