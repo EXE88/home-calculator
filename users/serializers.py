47dc1459440f2c98d4029af84be0689a87a76c12
danial <bobolk1388@gmail.com>
2023-09-08 13:32:30 +0430

    add required field for userloginserializer and userregisterserializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
 
 class UserLoginSerializers(serializers.ModelSerializer):
-    pass
+    username = serializers.CharField(max_length=50,min_length=5,required=True,allow_blank=False)
+    password = serializers.CharField(max_length=50,min_length=8,required=True,allow_blank=False)
 
 class UserRegisterSerializers(serializers.ModelSerializer):
-    pass
+    username = serializers.CharField(max_length=50,min_length=5,required=True,allow_blank=False)
+    email = serializers.EmailField(required=True,allow_blank=False)
+    password = serializers.CharField(max_length=50,min_length=8,required=True,allow_blank=False)
+    confirm_password = serializers.CharField(max_length=50,min_length=8,required=True,allow_blank=False)
+    
