6ecf1334e113091bb2a210dec0228da955accfdf
danial <bobolk1388@gmail.com>
2023-09-09 14:59:44 +0430

    adding exists user validate to validates

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login
 
 class UserLoginSerializers(serializers.ModelSerializer):
     class Meta:
@@ -16,12 +17,23 @@ class UserRegisterValidatePasswordSerializers(serializers.Serializer):
     confirm_password = serializers.CharField(max_length=50,min_length=8,required=True,allow_blank=False)
     
     def validate(self, attrs):
+        username = attrs.get("username")
+        email = attrs.get("email")
         password = attrs.get("password")
         confirm_password = attrs.get("confirm_password")
         
+        username_validateing = User.objects.filter(username=username).exists()
+        email_validateing = User.objects.filter(email=email).exists()
+        
+        if username_validateing:
+            raise serializers.ValidationError('there is a user with this username')
+        
+        if email_validateing:
+            raise serializers.ValidationError('there is a user whith this email')
+        
         if password != confirm_password:
             raise serializers.ValidationError('passwords is not match')
-        return password
+        return username
 
 class UserRegisterSerializers(serializers.Serializer):
     username = serializers.CharField(max_length=50,min_length=5,required=True,allow_blank=False)
--- users/views.py
+++ users/views.py
@@ -15,8 +15,8 @@ class UserLoginView(APIView):
 class UserRegisterView(APIView):
     def post(self,request):
         register_serializer = UserRegisterSerializers(data=request.data)
-        validate_password_serializer = UserRegisterValidatePasswordSerializers(data=request.data)
+        validate_password_serializer = UserRegisterValidatePasswordSerializers(data=request.data,context={"request":request})
         if validate_password_serializer.is_valid() and register_serializer.is_valid():
             register_serializer.save()
             return Response('user created',status=status.HTTP_200_OK)
-        return Response(register_serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        return Response(validate_password_serializer.errors,status=status.HTTP_400_BAD_REQUEST)
