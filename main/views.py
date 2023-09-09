2cc4eb308feafb18577507e11295986690ece7f0
danial <bobolk1388@gmail.com>
2023-09-10 00:49:15 +0430

    add permisons to main app views

--- main/views.py
+++ main/views.py
@@ -4,8 +4,10 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import ProjectInputs
 from .serializers import ProjectInputsSerializer,ProjectReviewSerializer
+from rest_framework import permissions
 
 class CreateNewProject(APIView):
+    permission_classes = [permissions.IsAuthenticated]
     def post(self, request):
         serializer = ProjectInputsSerializer(data=request.data)
         if serializer.is_valid():
@@ -14,6 +16,7 @@ class CreateNewProject(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
 class ProjectFullDetails(APIView):
+    permission_classes = [permissions.IsAuthenticated]
     def get_object(self, pk):
         try:
             return ProjectInputs.objects.get(pk=pk)
@@ -24,7 +27,7 @@ class ProjectFullDetails(APIView):
         project = self.get_object(pk)
         serializer = ProjectInputsSerializer(project)
         return Response(serializer.data,status=status.HTTP_200_OK)
-
+    
     def put(self, request, pk):
         project = self.get_object(pk)
         serializer = ProjectInputsSerializer(project, data=request.data)
@@ -39,6 +42,7 @@ class ProjectFullDetails(APIView):
         return Response(status=status.HTTP_200_OK)
     
 class ProjectReview(APIView):
+    permission_classes = [permissions.IsAuthenticated]
     def get(self,request,pk):
         try:
             project = ProjectInputs.objects.get(pk=pk)
