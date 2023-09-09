97a95591f69d867ea778bea852058a594dfccf82
danial <bobolk1388@gmail.com>
2023-09-10 00:57:08 +0430

    fix validate bug and but this time in register view

--- users/views.py
+++ users/views.py
@@ -7,22 +7,25 @@ from .serializers import UserLoginSerializers,UserLoginValidateSerializer,UserRe
 class UserLoginView(APIView):
     def post(self,request):
         login_serializer = UserLoginSerializers(data=request.data,context={"request":request})
-        validate_serializer = UserLoginValidateSerializer(data=request.data,context={"request":request})
-        if validate_serializer.is_valid():
+        validate_login_serializer = UserLoginValidateSerializer(data=request.data,context={"request":request})
+        if validate_login_serializer.is_valid():
             if login_serializer.is_valid():
                 login_serializer.save()
                 return Response('you are logedin successfully',status=status.HTTP_200_OK)
             else:
                 return Response(login_serializer.errors,status=status.HTTP_400_BAD_REQUEST)
         else:
-            return Response(validate_serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+            return Response(validate_login_serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
 class UserRegisterView(APIView):
     def post(self,request):
         register_serializer = UserRegisterSerializers(data=request.data)
-        validate_password_serializer = UserRegisterValidatePasswordSerializers(data=request.data,context={"request":request})
-        if validate_password_serializer.is_valid() and register_serializer.is_valid():
-            register_serializer.save()
-            return Response('you are registered successfully',status=status.HTTP_200_OK)
-        return Response(validate_password_serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-
+        validate_register_serializer = UserRegisterValidatePasswordSerializers(data=request.data,context={"request":request})
+        if validate_register_serializer.is_valid():
+            if register_serializer.is_valid():
+                register_serializer.save()
+                return Response('you are registered successfully',status=status.HTTP_200_OK)
+            else:
+                return Response(register_serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response(validate_register_serializer.errors,status=status.HTTP_400_BAD_REQUEST)
