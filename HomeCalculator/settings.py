0fdecfaa2e6c473d51a7c94c780a357d4e4e71e5
danial <bobolk1388@gmail.com>
2023-09-15 01:22:34 +0430

    set access token time to 60 minutes and refresh token to 7 days

--- HomeCalculator/settings.py
+++ HomeCalculator/settings.py
@@ -93,8 +93,8 @@ REST_FRAMEWORK = {
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
 }
 
 # Password validation
