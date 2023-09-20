f94137e2b16d72750c32a07ec1954c138600a08d
danial <bobolk1388@gmail.com>
2023-09-20 18:40:12 +0330

    add data extractor and data saver to views

--- price/urls.py
+++ price/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('update/',views.UpdatePrices.as_view(),name='update_price')
+    path('update/<int:category_id>/',views.UpdatePrices.as_view(),name='update_price')
 ]
