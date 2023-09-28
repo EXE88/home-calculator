c281d0880ae6291b8082ec535100a08243c21f81
danial <bobolk1388@gmail.com>
2023-09-28 23:05:22 +0330

    create data dictionary for sending all information in a response

--- main/views.py
+++ main/views.py
@@ -12,7 +12,10 @@ class CreateNewProject(APIView):
         serializer = ProjectInputsSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            data = {
+            "project details":serializer.data
+            }
+            return Response(data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
 class ProjectFullDetails(APIView):
@@ -26,7 +29,10 @@ class ProjectFullDetails(APIView):
     def get(self, request, pk):
         project = self.get_object(pk)
         serializer = ProjectInputsSerializer(project)
-        return Response(serializer.data,status=status.HTTP_200_OK)
+        data = {
+            "project details":serializer.data
+        }
+        return Response(data,status=status.HTTP_200_OK)
     
     def put(self, request, pk):
         project = self.get_object(pk)
