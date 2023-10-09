2c930cc744abc5139518383971cf793717fbdb61
danial <bobolk1388@gmail.com>
2023-10-09 22:06:16 +0330

    complete cement calculateing in project

--- main/views.py
+++ main/views.py
@@ -23,14 +23,21 @@ class Calculate:
         quantity_of_brick_in_west_and_east_wall_for_all_floors = quantity_of_brick_in_west_and_east_wall_for_one_floor*obj.floor
         quantity_of_brick_for_ceiling_in_one_floor = ((home_length*100)/brick_length)*((home_width*100)/brick_length)
         quantity_of_brick_for_ceiling_in_all_floors = quantity_of_brick_for_ceiling_in_one_floor*obj.floor
-        all_brick_objects = Material.objects.none()
-        all_brick_objects |= Material.objects.filter(name__icontains='آجر')
-        list_of_brick_objects = []
-        for brick in all_brick_objects:
-            object_details = (brick.name,brick.brand,brick.unit,brick.price,brick.last_price)
-            list_of_brick_objects.append(object_details)
-        current_price = list_of_brick_objects[0][3].replace('تومان','')
-        current_price = current_price.replace(',','')    
+        all_brick_objects = Material.objects.filter(name__icontains='آجر')
+        defult_calculate_object = Material.objects.filter(name__icontains='آجر فشاری در ابعاد 5*10*20')
+        list_of_objects = []
+        list_of_defult_objects = []
+        
+        for object in all_brick_objects:
+            object_details = (object.name,object.brand,object.unit,object.price,object.last_price)
+            list_of_objects.append(object_details)
+        for object in defult_calculate_object:
+            object_details = (object.name,object.brand,object.unit,object.price,object.last_price)
+            list_of_defult_objects.append(object_details)
+            
+        current_price = list_of_objects[0][3].replace('تومان','')
+        current_price = current_price.replace(',','')   
+         
         data = {
             'quantity of brick for one floor':{
                 'one layer':{
@@ -64,86 +71,180 @@ class Calculate:
                     'ceiling of entire building':quantity_of_brick_for_ceiling_in_all_floors  
                 }
             },
-            'types of brick':list_of_brick_objects,
-            'default of calculate':list_of_brick_objects[0],
+            'types of brick':list_of_objects,
+            'default of calculate':list_of_defult_objects,
             'costs':{
                 'for one floor':{
                     'one layer':{
-                        'north wall':float(quantity_of_brick_in_north_and_south_wall_for_one_floor)*float(current_price),
-                        'south wall':float(quantity_of_brick_in_north_and_south_wall_for_one_floor)*float(current_price),
-                        'west wall':float(quantity_of_brick_in_west_and_east_wall_for_one_floor)*float(current_price),
-                        'east wall':float(quantity_of_brick_in_west_and_east_wall_for_one_floor)*float(current_price),
-                        'ceiling':float(quantity_of_brick_for_ceiling_in_one_floor)*float(current_price)
+                        'north wall (تومان)':float(quantity_of_brick_in_north_and_south_wall_for_one_floor)*float(current_price),
+                        'south wall (تومان)':float(quantity_of_brick_in_north_and_south_wall_for_one_floor)*float(current_price),
+                        'west wall (تومان)':float(quantity_of_brick_in_west_and_east_wall_for_one_floor)*float(current_price),
+                        'east wall (تومان)':float(quantity_of_brick_in_west_and_east_wall_for_one_floor)*float(current_price),
+                        'ceiling (تومان)':float(quantity_of_brick_for_ceiling_in_one_floor)*float(current_price)
                     },
                     'two layer':{
-                        'north wall':(float(quantity_of_brick_in_north_and_south_wall_for_one_floor)*float(current_price))*2,
-                        'south wall':(float(quantity_of_brick_in_north_and_south_wall_for_one_floor)*float(current_price))*2,
-                        'west wall':(float(quantity_of_brick_in_west_and_east_wall_for_one_floor)*float(current_price))*2,
-                        'east wall':(float(quantity_of_brick_in_west_and_east_wall_for_one_floor)*float(current_price))*2,
-                        'ceiling':float(quantity_of_brick_for_ceiling_in_one_floor)*float(current_price) 
+                        'north wall (تومان)':(float(quantity_of_brick_in_north_and_south_wall_for_one_floor)*float(current_price))*2,
+                        'south wall (تومان)':(float(quantity_of_brick_in_north_and_south_wall_for_one_floor)*float(current_price))*2,
+                        'west wall (تومان)':(float(quantity_of_brick_in_west_and_east_wall_for_one_floor)*float(current_price))*2,
+                        'east wall (تومان)':(float(quantity_of_brick_in_west_and_east_wall_for_one_floor)*float(current_price))*2,
+                        'ceiling (تومان)':float(quantity_of_brick_for_ceiling_in_one_floor)*float(current_price) 
                     }
                 },
                 'for all floors':{
                     'one layer':{
-                        'north wall':float(quantity_of_brick_in_north_and_south_wall_for_all_floors)*float(current_price),
-                        'south wall':float(quantity_of_brick_in_north_and_south_wall_for_all_floors)*float(current_price),
-                        'west wall':float(quantity_of_brick_in_west_and_east_wall_for_all_floors)*float(current_price),
-                        'east wall':float(quantity_of_brick_in_west_and_east_wall_for_all_floors)*float(current_price),
-                        'ceiling':float(quantity_of_brick_for_ceiling_in_all_floors)*float(current_price)
+                        'north wall (تومان)':float(quantity_of_brick_in_north_and_south_wall_for_all_floors)*float(current_price),
+                        'south wall (تومان)':float(quantity_of_brick_in_north_and_south_wall_for_all_floors)*float(current_price),
+                        'west wall (تومان)':float(quantity_of_brick_in_west_and_east_wall_for_all_floors)*float(current_price),
+                        'east wall (تومان)':float(quantity_of_brick_in_west_and_east_wall_for_all_floors)*float(current_price),
+                        'ceiling (تومان)':float(quantity_of_brick_for_ceiling_in_all_floors)*float(current_price)
                     },
                     'two layer':{
-                        'north wall':(float(quantity_of_brick_in_north_and_south_wall_for_all_floors)*float(current_price))*2,
-                        'south wall':(float(quantity_of_brick_in_north_and_south_wall_for_all_floors)*float(current_price))*2,
-                        'west wall':(float(quantity_of_brick_in_west_and_east_wall_for_all_floors)*float(current_price))*2,
-                        'east wall':(float(quantity_of_brick_in_west_and_east_wall_for_all_floors)*float(current_price))*2,
-                        'ceiling':float(quantity_of_brick_for_ceiling_in_all_floors)*float(current_price)
+                        'north wall (تومان)':(float(quantity_of_brick_in_north_and_south_wall_for_all_floors)*float(current_price))*2,
+                        'south wall (تومان)':(float(quantity_of_brick_in_north_and_south_wall_for_all_floors)*float(current_price))*2,
+                        'west wall (تومان)':(float(quantity_of_brick_in_west_and_east_wall_for_all_floors)*float(current_price))*2,
+                        'east wall (تومان)':(float(quantity_of_brick_in_west_and_east_wall_for_all_floors)*float(current_price))*2,
+                        'ceiling (تومان)':float(quantity_of_brick_for_ceiling_in_all_floors)*float(current_price)
                     }
                 }
             }
         }
         return data
     def calculate_cement(self,pk,data):
+        obj = self.get_object(pk=pk)
+        floors = obj.floor
         cement_standard_size = 0.05
-        data = data['needed material']['brick']
-        one_floor_all_walls = data['quantity of brick for one floor']['one layer']
         north_wall_one_floor = data['quantity of brick for one floor']['one layer']['north wall of one floor']
         south_wall_one_floor = data['quantity of brick for one floor']['one layer']['south wall of one floor']
         west_wall_one_floor = data['quantity of brick for one floor']['one layer']['west wall of one floor']
         east_wall_one_floor = data['quantity of brick for one floor']['one layer']['east wall of one floor']
         ceiling_one_floor = data['quantity of brick for one floor']['one layer']['ceiling of one floor']
+        one_floor_all_walls = north_wall_one_floor+south_wall_one_floor+west_wall_one_floor+east_wall_one_floor+ceiling_one_floor
+        all_cement_objects = Material.objects.filter(name__icontains='سیمان')
+        defult_calculate_object = Material.objects.filter(name__icontains='سیمان پرتلند تیپ 2')
+        list_of_objects = []
+        list_of_defult_calculate_object = []
+        
+        for object in all_cement_objects:
+            object_detials = (object.name,object.brand,object.unit,object.price,object.last_price)
+            list_of_objects.append(object_detials)
+        for object in defult_calculate_object:
+            object_detials = (object.name,object.brand,object.unit,object.price,object.last_price)
+            list_of_defult_calculate_object.append(object_detials)
+            
+        current_price = list_of_objects[0][3].replace('تومان','')
+        current_price = current_price.replace(',','')   
+            
         data = {
-            'cement':{
-                'one floor':{
+            'one floor':{
+                'one layer':{
+                    'all':{
+                        'north wall need cement (kg)':(north_wall_one_floor*cement_standard_size),
+                        'south wall need cement (kg)':(south_wall_one_floor*cement_standard_size),
+                        'west wall need cement (kg)':(west_wall_one_floor*cement_standard_size),
+                        'east wall need cement (kg)':(east_wall_one_floor*cement_standard_size),
+                        'ceiling need cement (kg)':(ceiling_one_floor*cement_standard_size),
+                    },
+                    'total':{
+                        'total cement need (kg)':(one_floor_all_walls*cement_standard_size)
+                    }
+                },
+                'two layer':{
+                    'all':{
+                        'north wall need cement (kg)':(north_wall_one_floor*cement_standard_size)*2,
+                        'south wall need cement (kg)':(south_wall_one_floor*cement_standard_size)*2,
+                        'west wall need cement (kg)':(west_wall_one_floor*cement_standard_size)*2,
+                        'east wall need cement (kg)':(east_wall_one_floor*cement_standard_size)*2,
+                        'ceiling need cement (kg)':(ceiling_one_floor*cement_standard_size),
+                    },
+                    'total':{
+                        'total cement need (kg)':(((north_wall_one_floor+south_wall_one_floor+west_wall_one_floor+east_wall_one_floor)*cement_standard_size)*2)+(ceiling_one_floor*cement_standard_size)
+                    } 
+                }
+            },
+            'all floors':{
+               'one layer':{
+                   'all':{
+                        'north wall need cement (kg)':(north_wall_one_floor*cement_standard_size)*floors,
+                        'south wall need cement (kg)':(south_wall_one_floor*cement_standard_size)*floors,
+                        'west wall need cement (kg)':(west_wall_one_floor*cement_standard_size)*floors,
+                        'east wall need cement (kg)':(east_wall_one_floor*cement_standard_size)*floors,
+                        'ceiling need cement (kg)':(ceiling_one_floor*cement_standard_size)*floors,
+                   },
+                   'total':{
+                       'total cement need (kg)':(one_floor_all_walls*cement_standard_size)*floors
+                   }
+               },
+               'two layer':{
+                   'all':{
+                        'north wall need cement (kg)':((north_wall_one_floor*cement_standard_size)*2)*floors,
+                        'south wall need cement (kg)':((south_wall_one_floor*cement_standard_size)*2)*floors,
+                        'west wall need cement (kg)':((west_wall_one_floor*cement_standard_size)*2)*floors,
+                        'east wall need cement (kg)':((east_wall_one_floor*cement_standard_size)*2)*floors,
+                        'ceiling need cement (kg)':(ceiling_one_floor*cement_standard_size)*floors,
+                   },
+                   'total':{
+                       'total cement need (kg)':((((north_wall_one_floor+south_wall_one_floor+west_wall_one_floor+east_wall_one_floor)*cement_standard_size)*2)+(ceiling_one_floor*cement_standard_size))*floors
+                   }
+                }
+            },
+            'types of cement':list_of_objects,
+            'defult cement for calcualte':list_of_defult_calculate_object,
+            'costs':{
+                'for one floor':{
                     'one layer':{
                         'all':{
-                            'north wall need cement (kg)':(north_wall_one_floor*cement_standard_size),
-                            'south wall need cement (kg)':(south_wall_one_floor*cement_standard_size),
-                            'west wall need cement (kg)':(west_wall_one_floor*cement_standard_size),
-                            'east wall need cement (kg)':(east_wall_one_floor*cement_standard_size),
-                            'ceiling need cement (kg)':(ceiling_one_floor*cement_standard_size),
+                            'north wall (تومان)':float((north_wall_one_floor*cement_standard_size))*float(current_price),
+                            'south wall (تومان)':float((south_wall_one_floor*cement_standard_size))*float(current_price),
+                            'west wall (تومان)':float((west_wall_one_floor*cement_standard_size))*float(current_price),
+                            'east wall (تومان)':float((east_wall_one_floor*cement_standard_size))*float(current_price),
+                            'ceiling (تومان)':float((ceiling_one_floor*cement_standard_size))*float(current_price),
                         },
                         'total':{
-                            'total cement need (kg)':(one_floor_all_walls*cement_standard_size)
+                            'total price (تومان)':float((one_floor_all_walls*cement_standard_size))*float(current_price)
                         }
                     },
                     'two layer':{
                         'all':{
-                            'north wall need cement (kg)':(north_wall_one_floor*cement_standard_size)*2,
-                            'south wall need cement (kg)':(south_wall_one_floor*cement_standard_size)*2,
-                            'west wall need cement (kg)':(west_wall_one_floor*cement_standard_size)*2,
-                            'east wall need cement (kg)':(east_wall_one_floor*cement_standard_size)*2,
-                            'ceiling need cement (kg)':(ceiling_one_floor*cement_standard_size),
+                            'north wall (تومان)':float((north_wall_one_floor*cement_standard_size)*2)*float(current_price),
+                            'south wall (تومان)':float((south_wall_one_floor*cement_standard_size)*2)*float(current_price),
+                            'west wall (تومان)':float((west_wall_one_floor*cement_standard_size)*2)*float(current_price),
+                            'east wall (تومان)':float((east_wall_one_floor*cement_standard_size)*2)*float(current_price),
+                            'ceiling (تومان)':float((ceiling_one_floor*cement_standard_size))*float(current_price),
                         },
                         'total':{
-                            'total cement need (kg)':(one_floor_all_walls*cement_standard_size)*2
-                        } 
+                            'total price (تومان)':float((((north_wall_one_floor+south_wall_one_floor+west_wall_one_floor+east_wall_one_floor)*cement_standard_size)*2)+(ceiling_one_floor*cement_standard_size))*float(current_price)
+                        }
                     }
                 },
-                'all floors':{
-                    
+                'for all floors':{
+                    'one layer':{
+                        'all':{
+                            'north wall (تومان)':float((north_wall_one_floor*cement_standard_size)*floors)*float(current_price),
+                            'south wall (تومان)':float((south_wall_one_floor*cement_standard_size)*floors)*float(current_price),
+                            'west wall (تومان)':float((west_wall_one_floor*cement_standard_size)*floors)*float(current_price),
+                            'east wall (تومان)':float((east_wall_one_floor*cement_standard_size)*floors)*float(current_price),
+                            'ceiling (تومان)':float((ceiling_one_floor*cement_standard_size)*floors)*float(current_price),
+                        },
+                        'total':{
+                            'total price (تومان)':float((one_floor_all_walls*cement_standard_size)*floors)*float(current_price)
+                        }
+                    },
+                    'two layer':{
+                        'all':{
+                            'north wall (تومان)':float(((north_wall_one_floor*cement_standard_size)*2)*floors)*float(current_price),
+                            'south wall (تومان)':float(((south_wall_one_floor*cement_standard_size)*2)*floors)*float(current_price),
+                            'west wall (تومان)':float(((west_wall_one_floor*cement_standard_size)*2)*floors)*float(current_price),
+                            'east wall (تومان)':float(((east_wall_one_floor*cement_standard_size)*2)*floors)*float(current_price),
+                            'ceiling (تومان)':float((ceiling_one_floor*cement_standard_size)*floors)*float(current_price),
+                        },
+                        'total':{
+                            'total price (تومان)':float(((((north_wall_one_floor+south_wall_one_floor+west_wall_one_floor+east_wall_one_floor)*cement_standard_size)*2)+(ceiling_one_floor*cement_standard_size))*floors)*float(current_price)
+                        }
+                    }
                 }
             }
         }
+        return data
         
 
 class CreateNewProject(APIView):
@@ -189,10 +290,10 @@ class ProjectFullDetails(APIView):
                 data = {
                     "project details":project_details,  
                     "needed material":{
-                        'brick':Calculate.calculate_brick(self,pk=pk)
+                        'brick':Calculate.calculate_brick(self,pk=pk),
+                        'cement':Calculate.calculate_cement(self,pk=pk,data=Calculate.calculate_brick(self,pk=pk))
                     }  
                 }
-                Calculate.calculate_cement(self,pk=pk,data=data)
                 return Response(data,status=status.HTTP_200_OK)
     
     def put(self, request, pk):
